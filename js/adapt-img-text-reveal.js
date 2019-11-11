5e8c1a01f1830d27fffc263fe46520a5853f140d
saralsharma <saral.sharma@gmail.com>
2019-11-11 10:38:59 +0000

    Add files via upload

--- js/adapt-img-text-reveal.js
+++ js/adapt-img-text-reveal.js
@@ -6,72 +6,55 @@ define([
 
     var ImageTextRevealView = ComponentView.extend({
 
+        events:{
+          "click .graphic-widget img":"showHideText"
+        },
         preRender: function() {
             this.listenTo(Adapt, 'device:changed', this.resizeImage);
             this.checkIfResetOnRevisit();
-
         },
 
         postRender: function() {
-            this.resizeImage(Adapt.device.screenSize, true);
+            this.resizeImage(Adapt.device.screenSize);
             this.setReadyStatus();
-            // this.setupInview();
-            this.showHideText();
 
-            // this.setupInviewCompletion();
         },
-        // setupInview: function() {
-        //     var selector = this.getInviewElementSelector();
-        //     if (!selector) {
-        //         this.setCompletionStatus();
-        //         return;
-        //     }
-        //     this.setupInviewCompletion(selector);
-        // },
-        /**
-         * determines which element should be used for inview logic - body, instruction or title - and returns the selector for that element
-         */
-        getInviewElementSelector: function() {
-            if (this.model.get('body')) return '.component-body';
 
-            if (this.model.get('instruction')) return '.component-instruction';
-
-            if (this.model.get('displayTitle')) return '.component-title';
-
-            return null;
-        },
         checkIfResetOnRevisit: function() {
             var isResetOnRevisit = this.model.get('_isResetOnRevisit');
             if (isResetOnRevisit) {
                 this.model.reset(isResetOnRevisit);
             }
         },
-        resizeImage: function(width, setupInView) {
+        resizeImage: function(width) {
             var imageWidth = width === 'medium' ? 'small' : width;
             var imageSrc = (this.model.get('_graphic')) ? this.model.get('_graphic')[imageWidth] : '';
             this.$('.graphic-widget img').attr('src', imageSrc);
-
             this.$('.graphic-widget').imageready(function() {
                 this.setReadyStatus();
 
-                if (setupInView) {
-                    this.setupInviewCompletion('.component-widget');
-                }
             }.bind(this));
         },
-        showHideText: function(){
-            this.$(".graphic-widget img").click(function(){
-                var revealTxtSelector=$('.imageTextReveal-item-revealText');
-                if (revealTxtSelector.is(':visible')) revealTxtSelector.slideUp(this.duration); else {
-                    revealTxtSelector.slideDown(this.duration);
-                    this.setCompletionStatus();
+
+        showHideText: function(event){
+            var duration=this.model.get('duration');
+                event.preventDefault();
+                // var revealTxtSelector=$(event.currentTarget).parent().siblings('.imageTextReveal-item-revealText');
+                var revealTxtSelector=$("#"+this.model.get("_id")+"-revealTxt");
+
+                if (revealTxtSelector.is(':visible')) revealTxtSelector.slideUp(duration); else {
+                    revealTxtSelector.slideDown(duration);
+                    if (this.model.get('_setCompletionOn') === 'inview') {
+                        this.setupInviewCompletion('.component-widget');
+                    }
                 }
-        });
+
     }
 
     });
     var ImageTextRevealModel = ComponentModel.extend({
-        // implement your component model
+        // Model functions goes here
+
     });
     return Adapt.register('imageTextReveal', {
         model: ImageTextRevealModel,
